56de82a57ff309652ad244d58b7a352fda8982a6
Jared Beller <jbellerb@vt.edu>
2022-06-18 21:55:12 -0400

    more misc. tests

--- tests.sh
+++ tests.sh
@@ -6,26 +6,77 @@ print_usage() {
 }
 
 verify_test() {
-    TEST_NAME=${1#$TEST_DIR}
-    LOCATION=$(grep -HnoR -m 1 "$TEST_NAME" "$CORPUS")
-    if [ -n "$LOCATION" ]
+    TEST_NAME=${1#"$TEST_DIR"}
+    MATCHES=$(grep -HnoR "$TEST_NAME" "$CORPUS")
+    if [ -n "$MATCHES" ]
     then
         TEST_HASH=$(sha256 -q "$1")
         TEST_LENGTH=$(wc -l < "$1")
 
-        IFS=":" read -r FILE LINE SOURCE <<EOF
+        LOCATION=
+        EXTRAS=
+        for MATCH in $MATCHES
+        do
+            if [ -z "$LOCATION" ]
+            then
+                LOCATION=$MATCH
+            else
+                EXTRAS="$EXTRAS, $MATCH"
+            fi
+        done
+
+        if [ -z "$EXTRAS" ]
+        then
+            IFS=":" read -r FILE LINE _ <<EOF
 $LOCATION
 EOF
-        MATCH_RANGE="$((LINE + 3)),$((LINE + 2 + TEST_LENGTH))p"
-        MATCH_HASH=$(sed -n "$MATCH_RANGE" "$FILE" | sha256 -q)
+            MATCH_RANGE="$((LINE + 3)),$((LINE + 2 + TEST_LENGTH))p"
+            MATCH_HASH=$(sed -n "$MATCH_RANGE" "$FILE" | sha256 -q)
 
-        if [ $TEST_HASH != $MATCH_HASH ]
-        then
-            echo "mismatch: $TEST_NAME ($FILE)"
+            if [ "$TEST_HASH" != "$MATCH_HASH" ]
+            then
+                echo "mismatch: $TEST_NAME ($FILE)"
+            fi
+
+            # can't think of a better way to do this
+            FILE_HASH=$(echo "$FILE" | sha256 -q)
+            eval "VALID_${FILE_HASH}=\"$LINE\\n\$VALID_${FILE_HASH}\""
+        else
+            echo "duplicate: $LOCATION$EXTRAS"
         fi
     else
         echo "missing: $TEST_NAME"
     fi
+}
+
+find_extras() {
+    CANDIDATES=$(grep -hno "==================" "$1")
+    PREVIOUS="-2"
+
+    for CANDIDATE in $CANDIDATES
+    do
+        LINE=${CANDIDATE%:==================}
+        if [ $((LINE - PREVIOUS)) -eq 2 ]
+        then
+            LINE=$((LINE - 1))
+            FOUND=
+
+            for TEST in $2
+            do
+                if [ "$TEST" -eq "$LINE" ]
+                then
+                    FOUND=1
+                fi
+            done
+
+            if [ -z $FOUND ]
+            then
+                echo "unexpected: $1:$LINE:$(sed "${LINE}q;d" "$1")"
+            fi
+        else
+            PREVIOUS=$LINE
+        fi
+    done
 }
 
 while getopts "c:" name
@@ -55,4 +106,11 @@ TESTS=$(find "$TEST_DIR" -type f -name \*A.dhall)
 for TEST in $TESTS
 do
     verify_test "$TEST"
+done
+
+TEST_FILES=$(find "$CORPUS" -type f -name \*.txt)
+for TEST_FILE in $TEST_FILES
+do
+    FILE_HASH=$(echo "$TEST_FILE" | sha256 -q)
+    find_extras "$TEST_FILE" "$(eval "echo \"\$VALID_${FILE_HASH}\"")"
 done
